3833918c7d2e13b7b6eadee37a5112d7f4b1abc0
kiwoook <tizmfns1218@Naver.com>
2024-03-04 13:08:03 +0900

    Feat: 초기세팅

---
+++ sos_backend/src/main/java/com/study/sos_backend/SosBackendApplication.java
@@ -0,0 +1,13 @@
+package com.study.sos_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SosBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SosBackendApplication.class, args);
+    }
+
+}
---
+++ sos_backend/src/test/java/com/study/sos_backend/SosBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.sos_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SosBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
