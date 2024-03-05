37dcd50e1c6f92b2b72838d8da356a72c722fd08
kiwoook <tizmfns1218@Naver.com>
2024-03-05 17:14:55 +0900

    Feat: Swagger, Redis 추가

---
+++ sos_backend/src/main/java/com/study/sos_backend/config/SwaggerConfig.java
@@ -0,0 +1,25 @@
+package com.study.sos_backend.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI(){
+        return new OpenAPI()
+                .components(new Components())
+                .info(apiInfo());
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title("SOS API SPECIFICATION")
+                .description("SOS API 명세서")
+                .version("1.0.0");
+    }
+}
