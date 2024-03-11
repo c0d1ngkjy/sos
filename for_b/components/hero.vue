4da1fdce8559b1cffe213faf8678ccca6c02ccd6
c0d1ngkjy <108091177+c0d1ngkjy@users.noreply.github.com>
2024-03-11 18:25:55 +0900

    landing page for business

---
+++ for_b/components/hero.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="px-10 flex flex-col justify-center items-center text-center h-screen space-y-5">
+        <div class="md:text-6xl text-3xl font-bold">당신의 미용실을 <span class="text-primary">혁신</span>하세요.</div>
+        <div class="md:text-2xl text-md">예약 관리, 고객 만족, 그리고 비즈니스 성장을 위한 올인원 플랫폼.</div>
+        <UButton icon="i-heroicons-rocket-launch" size="xl" color="black" variant="solid" label="지금 신청하기"></UButton>
+
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+            class="w-6 h-6 animate-bounce absolute bottom-12">
+            <path stroke-linecap="round" stroke-linejoin="round"
+                d="m9 12.75 3 3m0 0 3-3m-3 3v-7.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+        </svg>
+    </div>
+</template>
---
+++ for_b/components/reviews.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="text-center py-28 px-10">
+        <UBadge color="primary" variant="solid">미용실에 신뢰받는</UBadge>
+        <div class="text-3xl font-bold pt-3 pb-16 text-center">고객 리뷰</div>
+
+        <div class="flex flex-col md:flex-row gap-5">
+            <div class="flex-1 flex flex-col items-center gap-4" v-for="review in reviews">
+                <!-- <UAvatar src="https://avatars.githubusercontent.com/u/739984?v=4" alt="Avatar" size="3xl" /> -->
+                <!-- <USkeleton class="h-32 w-32 animate-pulse" :ui="{ rounded: 'rounded-full' }" /> -->
+                <UIcon class="text-8xl" name="i-heroicons-user-circle" />
+
+                <div class="font-semibold">
+                    {{ review.label }}
+                </div>
+                <div class="text-slate-400">{{ review.content }}</div>
+            </div>
+
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+const reviews = [
+    { label: "시간을 아끼는 효율적인 예약 관리", content: "저희 미용실은 이 플랫폼을 사용하여 예약을 간단하게 관리하고 있습니다. 예약 일정을 확인하고 수정하는 것이 너무 쉬워서 운영에 큰 도움이 됩니다. 고객들도 쉽게 예약을 할 수 있어 만족도가 높아졌습니다." },
+    { label: "고객 만족도 급상승!", content: "예약 플랫폼을 사용한 후에는 고객들의 만족도가 급격히 상승했습니다. 예약 시스템이 신속하고 효율적이어서 고객들이 편리하게 예약할 수 있게 되었고, 자동화된 알림 기능 덕분에 예약 시간을 놓치지 않게 되었습니다. 이러한 서비스로 우리의 고객들은 더 이상 다른 곳을 찾지 않고 저희 미용실을 자주 이용하게 되었습니다." },
+    { label: "비즈니스 성장의 열쇠!", content: "저희 비즈니스는 이 플랫폼을 도입한 후에 급속한 성장을 이루어냈습니다. 예약 관리와 고객 만족도가 향상되어 많은 고객들이 저희 미용실을 선택하게 되었고, 이에 따라 매출도 상당히 증가했습니다. 또한 직원들의 업무 효율성도 향상되어 업무환경이 개선되었습니다. 이 플랫폼은 우리 비즈니스의 성장을 위한 필수 도구가 되었습니다." }
+]
+</script>
