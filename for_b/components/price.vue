da47f90e1921418f193e1a7ca80ca03387280a5a
c0d1ngkjy <108091177+c0d1ngkjy@users.noreply.github.com>
2024-03-12 12:07:07 +0900

    landing page - updt

--- for_b/components/price.vue
+++ for_b/components/price.vue
@@ -1,11 +1,13 @@
 <template>
-    <div class="flex flex-col gap-16 px-20 py-20">
+    <div class="flex flex-col gap-16 md:px-40 px-10 py-20">
         <div class="text-xl md:text-3xl font-bold text-center">간단 명료한 솔루션 플랜</div>
-        <div class="flex-col md:flex-row flex flex-grow gap-32 justify-between">
-            <div style="min-height: 400px;" v-for="plan in productPlans" class="px-8 space-y-4 py-4 border border-primary rounded-lg flex-1 flex flex-col items-start">
-                <UBadge class="text-xl" color="primary" variant="outline">{{ plan.name }}</UBadge>
+        <div class="flex-col md:flex-row flex flex-grow gap-24 justify-between">
+            <div :style="plan.name === '프로 플랜' ? '    background: linear-gradient(143deg, #8b5cf6, white);' : ''"
+                style="min-height: 400px;" v-for="plan in productPlans"
+                class="hover:shadow-md hover:shadow-primary px-8 space-y-4 py-4 border border-primary rounded-lg flex-1 flex flex-col items-start">
+                <div class="text-xl font-semibold">{{ plan.name }}</div>
                 <div class="text-3xl font-bold">{{ plan.price }}$ / 월</div>
-                
+
                 <div class="flex-grow flex flex-col justify-start gap-3">
                     <div v-for="item in plan.content" class="flex items-center gap-2">
                         <UIcon class="text-lg" name="i-heroicons-check-circle"></UIcon>
