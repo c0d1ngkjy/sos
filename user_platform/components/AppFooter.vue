78017ca173d6c91c26ff1065054dd3c31383aa0f
c0d1ngkjy <108091177+c0d1ngkjy@users.noreply.github.com>
2024-03-13 19:09:04 +0900

    ui update - userplatform

--- user_platform/components/AppFooter.vue
+++ user_platform/components/AppFooter.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="bg-gray-500 text-gray-700 h-52 md:px-10 px-4 py-2 text-right">
+    <div class="bg-gray-800 text-gray-300 h-52 md:px-10 px-4 py-2 text-right text-sm flex flex-col justify-end">
         <div>&copy;Salon Search</div>
         <div>codingkjy28@gmail.com</div>
     </div>
--- user_platform/nuxt.config.ts
+++ user_platform/nuxt.config.ts
@@ -8,10 +8,39 @@ export default defineNuxtConfig({
   plugins: [
   ],
   tailwindcss: {
-    cssPath: ['~/assets/css/tailwind.css', { injectionPosition: 'last' }],
+    cssPath: ['~/assets/css/tailwind.css', { injectPosition: 'last' }],
     configPath: 'tailwind.config',
     exposeConfig: false,
     config: {},
     viewer: true,
+  },
+  app: {
+    head: {
+      meta: [
+        // <meta name="viewport" content="width=device-width, initial-scale=1">
+        //{ name: 'viewport', content: 'width=device-width, initial-scale=1' }
+      ],
+      script: [
+        // <script src="https://myawesome-lib.js"></script>
+        //{ src: 'https://awesome-lib.js' }
+        // { src: 'https://developers.kakao.com/sdk/js/kakao.min.js' }
+        { src: 'https://t1.kakaocdn.net/kakao_js_sdk/2.6.0/kakao.min.js' }
+        //<script src="" integrity="sha384-6MFdIr0zOira1CHQkedUqJVql0YtcZA1P0nbPrQYJXVJZUkTk/oX4U9GhUIs3/z8" crossorigin="anonymous"></script>
+      ],
+      link: [
+        // <link rel="stylesheet" href="https://myawesome-lib.css">
+        //{ rel: 'stylesheet', href: 'https://awesome-lib.css' }
+      ],
+      // please note that this is an area that is likely to change
+      style: [
+        // <style type="text/css">:root { color: red }</style>
+        //{ children: ':root { color: red }', type: 'text/css' }
+      ],
+      noscript: [
+        // <noscript>JavaScript is required</noscript>
+        { children: 'JavaScript is required' }
+      ]
+    }
   }
+
 })
