1800fa3efbdc1030977d91d5b96da0df442d09b6
kiwoook <tizmfns1218@Naver.com>
2024-03-14 22:00:24 +0900

    Update SosBackendApplicationTests.java

--- sos_backend/src/test/java/com/study/sos_backend/SosBackendApplicationTests.java
+++ sos_backend/src/test/java/com/study/sos_backend/SosBackendApplicationTests.java
@@ -3,7 +3,7 @@ package com.study.sos_backend;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 class SosBackendApplicationTests {
 
     @Test
