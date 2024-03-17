d02d932bab075d6e7f4e1c137a49ebd404025af9
kiwoook <tizmfns1218@Naver.com>
2024-03-17 21:27:49 +0900

    Update SecurityConfig.java

--- sos_backend/src/main/java/com/study/sos_backend/config/SecurityConfig.java
+++ sos_backend/src/main/java/com/study/sos_backend/config/SecurityConfig.java
@@ -84,7 +84,7 @@ public class SecurityConfig {
         CorsConfiguration config = new CorsConfiguration();
 
         config.setAllowCredentials(true);
-        config.setAllowedOrigins(List.of("http://localhost:3000", "http://localhost:8080"));
+        config.setAllowedOrigins(List.of("http://localhost:3000", "http://localhost:8080", "http://43.201.246.72:8080"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setExposedHeaders(List.of("*"));
